eb98a937d07ef701228dffb9932fe667ad4c24c4
武佳琪 <294982684@qq.com>
2019-05-05 09:44:57 +0800

    build

--- src/components/home/music.vue
+++ src/components/home/music.vue
@@ -72,9 +72,9 @@
       },
       getPersonalized(){
 	      this.axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';  //此处是增加的代码，设置请求头的类型
-        this.axios.get(`http://musicapi.leanapp.cn/personalized`).then((response) => {
+        this.axios.get(`https://musicapi.leanapp.cn/personalized`).then((response) => {
           this.personalized = response.data.result;
-          this.listDetail = "http://musicapi.leanapp.cn/playlist/detail?id=" + response.data.result.id;
+          this.listDetail = "https://musicapi.leanapp.cn/playlist/detail?id=" + response.data.result.id;
         })
       }
     },
